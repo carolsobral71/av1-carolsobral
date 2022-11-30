8b9bdcd5eaa6039aafab43893880065dcc564d03
Carol <contatocarolsobral@uni9.edu.br>
2022-11-30 01:01:34 -0300

    primeiro teste

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace av1_carolsobral.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
